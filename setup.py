a742b1eff517979a69f869d56a9c96ac7ea63e81
Daehee Kim <daehee.kim@theloop.co.kr>
2018-08-22 15:46:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 from setuptools import setup, find_packages
 from earlgrey import __version__
 
-with open('requirements.txt') as requirements:
-    requires = list(requirements)
-
 setup_options = {
     'name': 'earlgrey',
-    'description': 'Message Queue library',
+    'description': 'Python AMQP RPC library',
+    'long_description': open('README.md').read(),
+    'long_description_content_type': 'text/markdown',
+    'url': 'https://github.com/icon-project/earlgrey',
     'version': __version__,
     'author': 'ICON foundation',
     'packages': find_packages(),
     'license': "Apache License 2.0",
-    'install_requires': requires,
+    'install_requires': list(open('requirements.txt')),
     'classifiers': [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -20,7 +20,8 @@ setup_options = {
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6.5'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ]
 }
 
